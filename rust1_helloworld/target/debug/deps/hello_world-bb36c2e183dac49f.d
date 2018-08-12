b6ba34bd0fa10f6a61858cd077cb3fc9fd5f7f20
WhenTheyCry96 <pajoheji0909@cex-trade.c.cextrade-202201.internal>
2018-08-12 14:29:16 +0000

    first

---
+++ rust1_helloworld/target/debug/deps/hello_world-bb36c2e183dac49f.d
@@ -0,0 +1,5 @@
+/home/pajoheji0909/rust_ws/rust1_helloworld/target/debug/deps/hello_world-bb36c2e183dac49f: src/main.rs
+
+/home/pajoheji0909/rust_ws/rust1_helloworld/target/debug/deps/hello_world-bb36c2e183dac49f.d: src/main.rs
+
+src/main.rs:
---
+++ rust1_helloworld/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/pajoheji0909/rust_ws/rust1_helloworld/target/debug/hello_world: /home/pajoheji0909/rust_ws/rust1_helloworld/src/main.rs
